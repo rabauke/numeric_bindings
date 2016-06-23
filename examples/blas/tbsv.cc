520a0caa87985a593aa704ed9dbce76a44b06c7c
Heiko Bauke <heiko.bauke@mail.de>
2016-06-23 13:42:49 +0200

    tbsv

--- examples/blas/tbsv.cc
+++ examples/blas/tbsv.cc
@@ -3,10 +3,10 @@
 #include <complex>
 #define BOOST_UBLAS_NO_ELEMENT_PROXIES
 #include <boost/numeric/ublas/vector.hpp>
-#include <boost/numeric/ublas/matrix.hpp>
+#include <boost/numeric/ublas/banded.hpp>
 #include <boost/numeric/ublas/triangular.hpp>
 #include <boost/numeric/bindings/ublas/vector.hpp>
-#include <boost/numeric/bindings/ublas/matrix.hpp>
+#include <boost/numeric/bindings/ublas/banded.hpp>
 #include <boost/numeric/bindings/blas/level2.hpp>
 #include <boost/numeric/bindings/lower.hpp>
 #include <boost/numeric/bindings/upper.hpp>
@@ -25,22 +25,15 @@ int main(int argc, char *argv[]) {
   {
     typedef std::complex<double> complex;
     typedef ublas::vector<complex> vector;
-    typedef ublas::matrix<complex, ublas::column_major> matrix;
-    typedef typename vector::size_type size_type;
+    typedef ublas::banded_matrix<complex, ublas::column_major> matrix;
+    typedef typename std::make_signed<vector::size_type>::type size_type;
     rand_normal<complex>::reset();
-    size_type n=8;
-    matrix A_l(n, n);
-    matrix A_u(n, n);
+    size_type n=8, k=2;
+    matrix A_u(n, n, 0, k), A_l(n, n, k, 0);
     for (size_type j=0; j<n; ++j) {
-      for (size_type i=0; i<j; ++i) {
-    	A_u(i, j)=rand_normal<complex>::get();
-	A_l(j, i)=rand_normal<complex>::get();
-      }
-      A_u(j, j)=rand_normal<complex>::get();
-      A_l(j, j)=rand_normal<complex>::get();
-      for (size_type i=j+1; i<n; ++i) {
-    	A_u(i, j)=0;
-	A_l(j, i)=0;
+      for (size_type i=std::max(size_type(0), j-k); i<=j; ++i) {
+        A_u(i, j)=rand_normal<complex>::get();
+        A_l(j, i)=rand_normal<complex>::get();
       }
     }
     vector x(n);
@@ -50,37 +43,37 @@ int main(int argc, char *argv[]) {
       vector x1(x);
       ublas::inplace_solve(A_l, x1, ublas::lower_tag());
       vector x2(x);
-      blas::trsv(blas::lower(A_l), x2);
+      blas::tbsv(k, blas::lower(A_l), x2);
       std::cout << "testing boost::ublas containers\n"
-		<< "using ublas (lower): " << print_vec(x1) << '\n'
-		<< "using blas  (lower): " << print_vec(x2) << '\n'
-		<< '\n';
+    		<< "using ublas (lower): " << print_vec(x1) << '\n'
+    		<< "using blas  (lower): " << print_vec(x2) << '\n'
+    		<< '\n';
     }
     {
       vector x1(x);
       ublas::inplace_solve(A_u, x1, ublas::upper_tag());
       vector x2(x);
-      blas::trsv(blas::upper(A_u), x2);
+      blas::tbsv(k, blas::upper(A_u), x2);
       std::cout << "testing boost::ublas containers\n"
-  		<< "using ublas (upper): " << print_vec(x1) << '\n'
-  		<< "using blas  (upper): " << print_vec(x2) << '\n'
-  		<< '\n';
+    		<< "using ublas (upper): " << print_vec(x1) << '\n'
+    		<< "using blas  (upper): " << print_vec(x2) << '\n'
+    		<< '\n';
     }
     {
       vector x1(x);
       ublas::inplace_solve(ublas::trans(A_l), x1, ublas::upper_tag());
       vector x2(x);
-      blas::trsv(blas::trans(blas::lower(A_l)), x2);
+      blas::tbsv(k, blas::trans(blas::lower(A_l)), x2);
       std::cout << "testing boost::ublas containers\n"
-  		<< "using ublas (trans, lower): " << print_vec(x1) << '\n'
-  		<< "using blas  (trans, lower): " << print_vec(x2) << '\n'
-  		<< '\n';
+    		<< "using ublas (trans, lower): " << print_vec(x1) << '\n'
+    		<< "using blas  (trans, lower): " << print_vec(x2) << '\n'
+    		<< '\n';
     }
     {
       vector x1(x);
       ublas::inplace_solve(ublas::trans(A_u), x1, ublas::lower_tag());
       vector x2(x);
-      blas::trsv(blas::trans(blas::upper(A_u)), x2);
+      blas::tbsv(k, blas::trans(blas::upper(A_u)), x2);
       std::cout << "testing boost::ublas containers\n"
     		<< "using ublas (trans, upper): " << print_vec(x1) << '\n'
     		<< "using blas  (trans, upper): " << print_vec(x2) << '\n'
@@ -90,7 +83,7 @@ int main(int argc, char *argv[]) {
       vector x1(x);
       ublas::inplace_solve(ublas::herm(A_l), x1, ublas::upper_tag());
       vector x2(x);
-      blas::trsv(blas::conj(blas::lower(A_l)), x2);
+      blas::tbsv(k, blas::conj(blas::lower(A_l)), x2);
       std::cout << "testing boost::ublas containers\n"
     		<< "using ublas (htrans, lower): " << print_vec(x1) << '\n'
     		<< "using blas  (htrans, lower): " << print_vec(x2) << '\n'
@@ -100,7 +93,7 @@ int main(int argc, char *argv[]) {
       vector x1(x);
       ublas::inplace_solve(ublas::herm(A_u), x1, ublas::lower_tag());
       vector x2(x);
-      blas::trsv(blas::conj(blas::upper(A_u)), x2);
+      blas::tbsv(k, blas::conj(blas::upper(A_u)), x2);
       std::cout << "testing boost::ublas containers\n"
     		<< "using ublas (htrans, upper): " << print_vec(x1) << '\n'
     		<< "using blas  (htrans, upper): " << print_vec(x2) << '\n'
@@ -116,37 +109,37 @@ int main(int argc, char *argv[]) {
       vector x1(x);
       ublas::inplace_solve(A_l, x1, ublas::unit_lower_tag());
       vector x2(x);
-      blas::trsv(blas::lower(A_l), x2);
+      blas::tbsv(k, blas::lower(A_l), x2);
       std::cout << "testing boost::ublas containers\n"
-		<< "using ublas (lower): " << print_vec(x1) << '\n'
-		<< "using blas  (lower): " << print_vec(x2) << '\n'
-		<< '\n';
+    		<< "using ublas (lower): " << print_vec(x1) << '\n'
+    		<< "using blas  (lower): " << print_vec(x2) << '\n'
+    		<< '\n';
     }
     {
       vector x1(x);
       ublas::inplace_solve(A_u, x1, ublas::unit_upper_tag());
       vector x2(x);
-      blas::trsv(blas::upper(A_u), x2);
+      blas::tbsv(k, blas::upper(A_u), x2);
       std::cout << "testing boost::ublas containers\n"
-  		<< "using ublas (upper): " << print_vec(x1) << '\n'
-  		<< "using blas  (upper): " << print_vec(x2) << '\n'
-  		<< '\n';
+    		<< "using ublas (upper): " << print_vec(x1) << '\n'
+    		<< "using blas  (upper): " << print_vec(x2) << '\n'
+    		<< '\n';
     }
     {
       vector x1(x);
       ublas::inplace_solve(ublas::trans(A_l), x1, ublas::unit_upper_tag());
       vector x2(x);
-      blas::trsv(blas::trans(blas::lower(A_l)), x2);
+      blas::tbsv(k, blas::trans(blas::lower(A_l)), x2);
       std::cout << "testing boost::ublas containers\n"
-  		<< "using ublas (trans, lower): " << print_vec(x1) << '\n'
-  		<< "using blas  (trans, lower): " << print_vec(x2) << '\n'
-  		<< '\n';
+    		<< "using ublas (trans, lower): " << print_vec(x1) << '\n'
+    		<< "using blas  (trans, lower): " << print_vec(x2) << '\n'
+    		<< '\n';
     }
     {
       vector x1(x);
       ublas::inplace_solve(ublas::trans(A_u), x1, ublas::unit_lower_tag());
       vector x2(x);
-      blas::trsv(blas::trans(blas::upper(A_u)), x2);
+      blas::tbsv(k, blas::trans(blas::upper(A_u)), x2);
       std::cout << "testing boost::ublas containers\n"
     		<< "using ublas (trans, upper): " << print_vec(x1) << '\n'
     		<< "using blas  (trans, upper): " << print_vec(x2) << '\n'
@@ -156,7 +149,7 @@ int main(int argc, char *argv[]) {
       vector x1(x);
       ublas::inplace_solve(ublas::herm(A_l), x1, ublas::unit_upper_tag());
       vector x2(x);
-      blas::trsv(blas::conj(blas::lower(A_l)), x2);
+      blas::tbsv(k, blas::conj(blas::lower(A_l)), x2);
       std::cout << "testing boost::ublas containers\n"
     		<< "using ublas (htrans, lower): " << print_vec(x1) << '\n'
     		<< "using blas  (htrans, lower): " << print_vec(x2) << '\n'
@@ -166,7 +159,7 @@ int main(int argc, char *argv[]) {
       vector x1(x);
       ublas::inplace_solve(ublas::herm(A_u), x1, ublas::unit_lower_tag());
       vector x2(x);
-      blas::trsv(blas::conj(blas::upper(A_u)), x2);
+      blas::tbsv(k, blas::conj(blas::upper(A_u)), x2);
       std::cout << "testing boost::ublas containers\n"
     		<< "using ublas (htrans, upper): " << print_vec(x1) << '\n'
     		<< "using blas  (htrans, upper): " << print_vec(x2) << '\n'

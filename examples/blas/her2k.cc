82fff1b621c52a3fafc38260a759a1eb15506570
Heiko Bauke <heiko.bauke@mail.de>
2016-06-28 17:19:00 +0200

    her2k

--- examples/blas/her2k.cc
+++ examples/blas/her2k.cc
@@ -20,10 +20,12 @@ int main(int argc, char *argv[]) {
     typedef matrix::size_type size_type;
     rand_normal<complex>::reset();
     size_type n=8;
-    matrix A(n, n), C(n, n);
+    matrix A(n, n), B(n, n), C(n, n);
     for (size_type j=0; j<n; ++j)
-      for (size_type i=0; i<n; ++i)
+      for (size_type i=0; i<n; ++i) {
 	A(i, j)=rand_normal<complex>::get();
+	B(i, j)=rand_normal<complex>::get();
+      }
     for (size_type j=0; j<n; ++j) {
       for (size_type i=0; i<j; ++i) {
 	C(i, j)=rand_normal<complex>::get();
@@ -31,51 +33,59 @@ int main(int argc, char *argv[]) {
       }
       C(j, j)=rand_normal<complex>::get().real();
     }
-    double alpha(rand_normal<complex>::get().real());
+    complex alpha(rand_normal<complex>::get());
     double beta(rand_normal<complex>::get().real());
     {
-      matrix C1(alpha*ublas::prod(A, ublas::trans(ublas::conj(A)))+beta*C);
+      matrix C1(alpha*ublas::prod(A, ublas::trans(ublas::conj(B)))+
+		std::conj(alpha)*ublas::prod(B, ublas::trans(ublas::conj(A)))+
+		beta*C);
       for (size_type j=0; j<n; ++j)
 	for (size_type i=j+1; i<n; ++i)
 	  C1(i, j)=C(i, j);
       matrix C2(C);
-      blas::herk(alpha, A, beta, blas::upper(C2));
+      blas::her2k(alpha, A, B, beta, blas::upper(C2));
       std::cout << "testing boost::ublas containers\n"
 		<< "using ublas (A right htrans, C upper):\n" << print_mat(C1) << '\n'
 		<< "using blas (A right htrans, C upper):\n" << print_mat(C2) << '\n'
 		<< '\n';
     }
     {
-      matrix C1(alpha*ublas::prod(A, ublas::trans(ublas::conj(A)))+beta*C);
+      matrix C1(alpha*ublas::prod(A, ublas::trans(ublas::conj(B)))+
+		std::conj(alpha)*ublas::prod(B, ublas::trans(ublas::conj(A)))+
+		beta*C);
       for (size_type j=0; j<n; ++j)
     	for (size_type i=0; i<j; ++i)
     	  C1(i, j)=C(i, j);
       matrix C2(C);
-      blas::herk(alpha, A, beta, blas::lower(C2));
+      blas::her2k(alpha, A, B, beta, blas::lower(C2));
       std::cout << "testing boost::ublas containers\n"
     		<< "using ublas (A right htrans, C lower):\n" << print_mat(C1) << '\n'
     		<< "using blas (A right htrans, C lower):\n" << print_mat(C2) << '\n'
     		<< '\n';
     }
     {
-      matrix C1(alpha*ublas::prod(ublas::trans(ublas::conj(A)), A)+beta*C);
+      matrix C1(alpha*ublas::prod(ublas::trans(ublas::conj(A)), B)+
+		std::conj(alpha)*ublas::prod(ublas::trans(ublas::conj(B)), A)+
+		beta*C);
       for (size_type j=0; j<n; ++j)
     	for (size_type i=j+1; i<n; ++i)
     	  C1(i, j)=C(i, j);
       matrix C2(C);
-      blas::herk(alpha, blas::conj(A), beta, blas::upper(C2));
+      blas::her2k(alpha, blas::conj(A), B, beta, blas::upper(C2));
       std::cout << "testing boost::ublas containers\n"
     		<< "using ublas (A left htrans, C upper):\n" << print_mat(C1) << '\n'
     		<< "using blas (A left htrans, C upper):\n" << print_mat(C2) << '\n'
     		<< '\n';
     }
     {
-      matrix C1(alpha*ublas::prod(ublas::trans(ublas::conj(A)), A)+beta*C);
+      matrix C1(alpha*ublas::prod(ublas::trans(ublas::conj(A)), B)+
+		std::conj(alpha)*ublas::prod(ublas::trans(ublas::conj(B)), A)+
+		beta*C);
       for (size_type j=0; j<n; ++j)
     	for (size_type i=0; i<j; ++i)
     	  C1(i, j)=C(i, j);
       matrix C2(C);
-      blas::herk(alpha, blas::conj(A), beta, blas::lower(C2));
+      blas::her2k(alpha, blas::conj(A), B, beta, blas::lower(C2));
       std::cout << "testing boost::ublas containers\n"
     		<< "using ublas (A left htrans, C lower):\n" << print_mat(C1) << '\n'
     		<< "using blas (A left htrans, C lower):\n" << print_mat(C2) << '\n'

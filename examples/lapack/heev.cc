3795794627de876885db60869949cf112339a087
Heiko Bauke <heiko.bauke@mail.de>
2016-07-07 23:11:01 +0200

    heev

--- examples/lapack/heev.cc
+++ examples/lapack/heev.cc
@@ -9,7 +9,7 @@
 #include <boost/numeric/bindings/ublas/matrix_proxy.hpp>
 #include <boost/numeric/bindings/blas/level1.hpp>
 #include <boost/numeric/bindings/blas/level2.hpp>
-#include <boost/numeric/bindings/conj.hpp>
+#include <boost/numeric/bindings/upper.hpp>
 #include <boost/numeric/bindings/lapack/driver.hpp>
 #include <boost/numeric/ublas/io.hpp>
 #include "random.hpp"
@@ -21,50 +21,28 @@ namespace lapack=boost::numeric::bindings::lapack;
 int main(int argc, char *argv[]) {
   typedef std::complex<double> complex;
   typedef ublas::vector<complex> vector;
+  typedef ublas::vector<double> d_vector;
   typedef ublas::matrix<complex, ublas::column_major> matrix;
   typedef typename vector::size_type size_type;
 
   rand_normal<complex>::reset();
   int n=128;
   matrix A(n, n);
-  for (size_type j=0; j<n; ++j)
-    for (size_type i=0; i<n; ++i)
+  for (size_type j=0; j<n; ++j) {
+    for (size_type i=0; i<j; ++i) {
       A(i, j)=rand_normal<complex>::get();
-  {
-    vector lambda(n);
-    matrix vl(n, n), vr(n, n), A_bak(A);
-    int info=lapack::geev('V', 'V', A, lambda, vl, vr);
-    if (info==0) {
-      for (int i=0; i<n; ++i) {
-	// res <- A*vr(i) - lambda(i)*vr(i)
-	ublas::matrix_column<matrix> v(vr, i);
-	vector res(v);
-	blas::gemv(complex(1), A_bak, v, -lambda(i), res);
-	std::cout << "norm of residual (right eigen vector " << i
-		  << " ): " << blas::nrm2(res) << '\n';
-      }
-      for (int i=0; i<n; ++i) {
-	// res <- ( vl(i)^H*A - vl(i)^H*lambda(i) )^H
-	ublas::matrix_column<matrix> v(vl, i);
-	vector res(v);
-	blas::gemv(complex(1), blas::conj(A_bak), v, -std::conj(lambda(i)), res);
-	std::cout << "norm of residual (left eigen vector " << i
-		  << " ): " << blas::nrm2(res) << '\n';
-      }
-    } else
-      if (info>0)
-	std::cout << "unable to compute all eigen values\n";
-      else 
-	std::cout << "illegal arguments\n";
+      A(j, i)=std::conj(A(i, j));
+    }
+    A(j, j)=rand_normal<complex>::get().real();
   }
   {
-    vector lambda(n);
-    matrix dummy(1, 1), vr(n, n), A_bak(A);
-    int info=lapack::geev('N', 'V', A, lambda, dummy, vr);
+    d_vector lambda(n);
+    matrix A_bak(A);
+    int info=lapack::heev('V', lapack::upper(A), lambda);
     if (info==0) {
       for (int i=0; i<n; ++i) {
 	// res <- A*vr(i) - lambda(i)*vr(i)
-	ublas::matrix_column<matrix> v(vr, i);
+	ublas::matrix_column<matrix> v(A, i);
 	vector res(v);
 	blas::gemv(complex(1), A_bak, v, -lambda(i), res);
 	std::cout << "norm of residual (right eigen vector " << i
@@ -78,4 +56,3 @@ int main(int argc, char *argv[]) {
   }
   return EXIT_SUCCESS;
 }
-
